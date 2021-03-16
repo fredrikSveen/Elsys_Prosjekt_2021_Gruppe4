8e581943bd92277a90ebbd0c19a5fc2964408042
Mina <minamsv@stud.ntnu.no>
2021-03-16 14:02:09 +0100

    Nedtelling av steiner

--- GUI/GUIScreen.py
+++ GUI/GUIScreen.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import *
+from tkinter import Button, Label, Tk, Entry, END
 
 window = tk.Tk()
 window.title("Curlingspill")
@@ -10,7 +10,10 @@ window.geometry('800x480')
 runder = 5
 rundenr = 1
 avsluttBool = False
-    
+stones = 0
+totalStones = 4
+stones1 = int(totalStones/2)
+stones2 = int(totalStones/2)
 
 #Funksjoner
 #roundKeeper(tar inn data fra pi-en):
@@ -42,6 +45,9 @@ def Avslutt():
     tilbake.place(relx = 0.2, rely = 0.4)
 
 def window1():
+    x = tk.Label(window, text="Hvor mange runder vil dere spille?", font=("Arial Bold", 20))
+    x.place(relx = 0.3, rely = 0.2)
+
     l = tk.Label(window, text=str(runder), font=("Arial Bold", 60))
     l.place(relx = 0.4, rely = 0.42)
     
@@ -64,10 +70,10 @@ def window1():
         window2()
         
     
-    opp = tk.Button(window, text="Pil opp", command=pilOpp, font=("Arial Bold", 30))
+    opp = tk.Button(window, text="\u2191", command=pilOpp, font=("Arial Bold", 30))
     opp.place(relx = 0.5, rely = 0.4)
     
-    ned = tk.Button(window, text="Pil ned", command=pilNed, font=("Arial Bold", 30))
+    ned = tk.Button(window, text="\u2193", command=pilNed, font=("Arial Bold", 30))
     ned.place(relx = 0.5, rely = 0.5)
     
     start = tk.Button(window, text="Start", command=Start, font=("Arial Bold", 25))
@@ -85,45 +91,42 @@ def window2():
     lag2 = tk.Label(window, text="Team 2", font=("Arial bold", 40)) 
     lag2.place(relx = 0.5, rely = 0.2)
 
-    
-
     #simulasjon av steinkast
-    totalStones = 4
-
-    stones = 0
     stones1 = int(totalStones/2)
     stones2 = int(totalStones/2)
-
-    team1stones = tk.Label(window, text=f"{stones1} steiner igjen", font=("Arial bold", 30))
+    
+    team1stones = tk.Label(window, text=str(stones1), font=("Arial bold", 30))
     team1stones.place(relx = 0, rely = 0.4)
 
-    team2stones = tk.Label(window, text=f"{stones2} steiner igjen", font=("Arial bold", 30))
+    team2stones = tk.Label(window, text=str(stones2), font=("Arial bold", 30))
     team2stones.place(relx = 0.5, rely = 0.4)
 
     def s():
         global stones
         stones+=1
+        value1 = int(team1stones["text"])
+        value2 = int(team2stones["text"])
+        if (stones % 2 == 0):
+            global stones2
+            stones2 -= 1
+            team2stones["text"] = str(value2 - 1)
+
+        else:
+            global stones1
+            stones1 -= 1
+            team1stones["text"] = str(value1 - 1)
+
         if (stones == totalStones):
             stones = 0
             global rundenr
             rundenr+=1
             clearFrame()
             window3()
-            if (stones % 2 == 0):
-                global stones2
-                stones2 -= 1
-                team2stones["text"] = f"{stones2} steiner igjen"
-            else:
-                global stones1
-                stones1 -= 1
-                team1stones["text"] = f"{stones1} steiner igjen"
-
             
     stonesButton=tk.Button(window, text="Steiner", command=s)
     stonesButton.place(relx = 0.5, rely = 0.5)
 
-    
-    
+        
 def window3():
     def w4():
         clearFrame()
@@ -195,8 +198,8 @@ def window4():
 
     vinner = "Team 1" # legge inn den faktiske vinneren
 
-    vinnerText = tk.Label(window, text="Vinneren er "+vinner, font=("Arial Bold", 30))
-    vinnerText.place(relx=0.3, rely=0.3)
+    vinnerText = tk.Label(window, text="Vinneren er "+vinner, font=("Arial Bold", 50))
+    vinnerText.place(relx=0.2, rely=0.3)
 
 
 window1()
