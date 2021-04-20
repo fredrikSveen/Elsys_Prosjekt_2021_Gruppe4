90e76257fa7f84bcbebe3bc736e08868e44152ab
Mina <minamsv@stud.ntnu.no>
2021-04-20 13:43:01 +0200

    Vet ikke

--- GUI/GUIScreen.py
+++ GUI/GUIScreen.py
@@ -280,8 +280,8 @@ def window4():
         clearFrame()
         window1()
         for i in range(1,len(table)):
-            table[i][1] = '0'
-            table[i][2] = '0'
+            table[i][1] = ''
+            table[i][2] = ''
     
     global rundenr
     rundenr = 1
