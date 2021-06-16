d50456177ff9013bb573004b662fe215ca74d95b
Fredrik <fre-svee@hotmail.no>
2021-06-16 12:39:25 +0200

    a try

--- GUI/GUIScreen.py
+++ GUI/GUIScreen.py
@@ -234,7 +234,9 @@ def window1(): # Åpner første vindu
         clearFrame()
         window2()   
 
-    opp = Button(window, text=u'\u2B99', command=pilOpp, font=("Arial Bold", 30), bg = knapp, activebackground = aktivknapp) # Oppknapp
+    unicode_char = '\u2B99'
+
+    opp = Button(window, text=u'\u2B99'.format(unicode_char = unicode_char), command=pilOpp, font=("Arial Bold", 30), bg = knapp, activebackground = aktivknapp).pack() # Oppknapp
     opp.place(relx = 0.3, rely = 0.35)
     
     ned = Button(window, text="\u2B9b", command=pilNed, font=("Arial Bold", 30),bg = knapp, activebackground = aktivknapp) # Nedknapp
